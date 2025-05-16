dbad12634bb7a77c224ddfb22007b057e6c37f68
Elias.Egel <elias.egelrid@gmail.com>
2025-05-16 13:03:12 +0200

    made the pokemon page route able to show pokemon

--- pokedex-explorer/src/routes/$pokemon.tsx
+++ pokedex-explorer/src/routes/$pokemon.tsx
@@ -1,9 +1,22 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { usePokemon } from '../components/usePokemon';
 
 export const Route = createFileRoute('/$pokemon')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/$pokemon"!</div>
+  const { pokemon } = Route.useParams();
+  const { data, isPending, error } = usePokemon(pokemon);
+
+  if (isPending) return <p>Loading...</p>;
+  if (error) return <p>Error: {(error as Error).message}</p>;
+
+  return (
+    <div>
+      <h1 className="capitalize text-3xl font-bold">{data.name}</h1>
+      <img src={data.sprites.front_default} alt={data.name} />
+      {/* Additional details */}
+    </div>
+  );
 }
