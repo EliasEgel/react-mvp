ea18dcdebad9cfb1ab6d82692b4adf0b32610372
Elias.Egel <elias.egelrid@gmail.com>
2025-05-16 15:40:44 +0200

    made favorites page able to show cards based on the local storage

--- pokedex-explorer/src/routes/favorites.tsx
+++ pokedex-explorer/src/routes/favorites.tsx
@@ -1,9 +1,22 @@
 import { createFileRoute } from "@tanstack/react-router";
+import PokemonCard from "../components/PokemonCard";
 
 export const Route = createFileRoute("/favorites")({
   component: Favorites,
 });
 
 function Favorites() {
-  return <div className="bg-[#f1d5ce]">Hello "/favorites"!</div>;
+  const favorites = JSON.parse(localStorage.getItem("savedPokemon") || "[]");
+  return (
+    <div className="flex flex-col items-center gap-4 px-4 py-8 text-center bg-[#f1d5ce]">
+      <h1 className="capitalize text-3xl font-bold text-[#c85250]">
+        your Favorites
+      </h1>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-20">
+        {favorites.map((pokemon) => (
+          <PokemonCard key={pokemon} name={pokemon} />
+        ))}
+      </div>
+    </div>
+  );
 }
